335e418549696c75be6a1966d560cd0644d2ddda
Stephan Gopaul <stephangopaul@gmail.com>
2023-02-10 16:16:03 +0400

    added missing imports

--- __brick__/{{filename.snakeCase()}}_view.dart
+++ __brick__/{{filename.snakeCase()}}_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:{{project_name}}/ui/views/base_view.dart';
+import 'package:{{project_name}}/core/viewmodels/{{filename.lowerCase()}}_vm.dart';
 
 class {{filename.pascalCase()}}View extends StatelessWidget {
  const {{filename.pascalCase()}}View({super.key});
--- __brick__/{{filename.snakeCase()}}_vm.dart
+++ __brick__/{{filename.snakeCase()}}_vm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:{{project_name}}/core/viewmodels/base_vm.dart';
 
 class {{filename.pascalCase()}}VM extends BaseVM {
   init(){}
